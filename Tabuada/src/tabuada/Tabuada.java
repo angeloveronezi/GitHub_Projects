f201f2fbaabdf7b3ef4478f35f4edabea822a607
Angelo Budil Veronezi <abveronezi@yahoo.com.br>
2020-04-25 00:56:11 -0300

    Tabuada
    Projeto básico de uma tela para tabuada.

---
+++ Tabuada/src/tabuada/Tabuada.java
@@ -0,0 +1,14 @@
+/*
+ * Criando uma tabuada
+ */
+package tabuada;
+import java.util.Scanner;
+
+public class Tabuada {
+
+    public static void main(String[] args) {
+        TelaPrincipal tela = new TelaPrincipal ();
+        tela.setVisible(true);
+    }
+    
+}
